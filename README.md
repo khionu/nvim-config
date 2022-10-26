f3535a95400215d3bc70acddfe59fc4794def654
Khionu Sybiern <dev@khionu.net>
2022-10-25 17:29:34 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# My NeoVim
+
+This is my config. It'll grow over time. If you made use of it, referencing or ripping, please throw it a star!
---
+++ init.lua
@@ -0,0 +1,84 @@
+vim.opt.number = true
+vim.opt.mouse = 'a'
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.hlsearch = false
+vim.opt.wrap = true
+vim.opt.breakindent = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+
+local install_path = vim.fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+local install_plugins = false
+
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+  print('Installing packer...')
+  local packer_url = 'https://github.com/wbthomason/packer.nvim'
+  vim.fn.system({'git', 'clone', '--depth', '1', packer_url, install_path})
+  print('Done.')
+
+  vim.cmd('packadd packer.nvim')
+  install_plugins = true
+end
+
+require('packer').startup(function(use)
+  -- Package manager
+  use 'wbthomason/packer.nvim'
+
+  -- Theme
+  use 'rakr/vim-one'
+
+  use 'nvim-treesitter/nvim-treesitter'
+
+  use {
+    'VonHeikemen/lsp-zero.nvim',
+    requires = {
+      -- LSP Support
+      {'neovim/nvim-lspconfig'},
+      {'williamboman/mason.nvim'},
+      {'williamboman/mason-lspconfig.nvim'},
+
+      -- Autocompletion
+      {'hrsh7th/nvim-cmp'},
+      {'hrsh7th/cmp-buffer'},
+      {'hrsh7th/cmp-path'},
+      {'saadparwaiz1/cmp_luasnip'},
+      {'hrsh7th/cmp-nvim-lsp'},
+      {'hrsh7th/cmp-nvim-lua'},
+
+      -- Snippets
+      {'L3MON4D3/LuaSnip'},
+      {'rafamadriz/friendly-snippets'},
+    }
+  }
+
+  if install_plugins then
+    require('packer').sync()
+  end
+end)
+
+if install_plugins then
+  return
+end
+
+vim.opt.termguicolors = true
+
+vim.cmd('colorscheme one')
+vim.cmd('set background=dark')
+vim.cmd('let g:one_allow_italics = 1')
+
+local treesitter = require('nvim-treesitter.configs')
+
+treesitter.setup({
+  ensure_installed = "all",
+  highlight = {
+    enable = true,
+  }
+})
+
+local lsp = require('lsp-zero')
+
+lsp.preset('recommended')
+lsp.setup()
+
